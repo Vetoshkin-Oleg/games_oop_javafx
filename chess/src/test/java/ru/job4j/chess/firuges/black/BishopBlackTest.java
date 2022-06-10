2b9a2cecc2136363f4767ea1240b40cba22172a8
Vetoshkin-Oleg <voa2014@google.com>
2022-06-10 22:30:54 +0500

    2. Каркас шахматной доски [#792 #310661]

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -17,11 +17,53 @@ public class BishopBlackTest {
     }
 
     @Test
-    public void way() {
+    public void wayFromD4ToH8() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.D4);
+        Cell[] way = bishopBlack.way(Cell.H8);
+        Cell[] expected = new Cell[] {Cell.E5, Cell.F6, Cell.G7, Cell.H8};
+        assertEquals(expected, way);
     }
 
     @Test
-    public void isDiagonal() {
+    public void wayFromD4ToA1() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.D4);
+        Cell[] way = bishopBlack.way(Cell.A1);
+        Cell[] expected = new Cell[] {Cell.C3, Cell.B2, Cell.A1};
+        assertEquals(expected, way);
+    }
+
+    @Test
+    public void wayFromD4ToA7() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.D4);
+        Cell[] way = bishopBlack.way(Cell.A7);
+        Cell[] expected = new Cell[] {Cell.C5, Cell.B6, Cell.A7};
+        assertEquals(expected, way);
+    }
+
+    @Test
+    public void wayFromD4ToG1() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.D4);
+        Cell[] way = bishopBlack.way(Cell.G1);
+        Cell[] expected = new Cell[] {Cell.E3, Cell.F2, Cell.G1};
+        assertEquals(expected, way);
+    }
+
+    @Test
+    public void isDiagonalFromD4ToH8() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.D4);
+        Cell source = bishopBlack.position();
+        Cell dest = Cell.H8;
+        boolean isDiagonal = bishopBlack.isDiagonal(source, dest);
+        assertTrue(isDiagonal);
+    }
+
+    @Test
+    public void isNotDiagonalFromD4ToB7() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.D4);
+        Cell source = bishopBlack.position();
+        Cell dest = Cell.B7;
+        boolean isDiagonal = bishopBlack.isDiagonal(source, dest);
+        assertFalse(isDiagonal);
     }
 
     @Test
